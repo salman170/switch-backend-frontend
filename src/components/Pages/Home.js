445675a37c5c2518d744f775e4458d6d98c0b59e
salman170 <salmanansari02120@gmail.com>
2023-03-08 18:06:35 +0530

    working on video on home page

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import vBUSES from "../../assets/video/vBUSES.mp4"
 
 const Home = () => {
   return (
     <div>
-      This is Home
+      <section class="video-container">
+          <video
+            width="100%"
+            preload="metadata"
+            loop autoPlay
+            playsinline
+            muted
+            class="d-block"
+          >
+            <source
+              src={vBUSES}
+              type="video/mp4"
+            />
+
+            Your browser does not support the video tag.
+          </video>
+        </section>
     </div>
   )
 }
