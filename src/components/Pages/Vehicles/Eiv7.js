c801ad39bc9a91fc8925ff011854798ee08b2cd8
salman170 <salmanansari02120@gmail.com>
2023-03-25 17:48:30 +0530

    working on image slider for eiv7

--- src/components/Pages/Vehicles/Eiv7.js
+++ src/components/Pages/Vehicles/Eiv7.js
@@ -4,14 +4,29 @@ import homeBanner from '../../../assets/themes/custom/switchmobility/resources/i
 import homeMobileBanner from '../../../assets/themes/custom/switchmobility/resources/images/eiv7/EiV-mobile-banner.jpg';
 import logoEiv from '../../../assets/sites/default/files/eiv7/eiv-logo-sm.png';
 import productEiv7 from '../../../assets/sites/default/files/eiv7/Eiv-product-offering-image.jpg';
+import eivImage1 from '../../../assets/sites/default/files/eiv7/EiV-img1.jpg';
+import eivImage2 from '../../../assets/sites/default/files/eiv7/EiV-img2.jpg';
+import eivImage3 from '../../../assets/sites/default/files/eiv7/EiV-img3.jpg';
+import eivImage4 from '../../../assets/sites/default/files/eiv7/EiV-img4.jpg';
+import eivImage5 from '../../../assets/sites/default/files/eiv7/EiV-img5.jpg';
 
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import ImageSlider from './imageSlider';
+
+const slides = [
+  { url: {eivImage1}, title: 'beach' },
+  { url: {eivImage2}, title: 'boat' },
+  { url: {eivImage3}, title: 'forest' },
+  { url: {eivImage4}, title: 'city' },
+  { url: {eivImage5}, title: 'italy' },
+];
 
 const Eiv7 = () => {
   useEffect(() => {
     AOS.init();
   }, []);
+
   return (
     <div>
       <section>
@@ -71,29 +86,34 @@ const Eiv7 = () => {
         data-aos-duration='1500'
       >
         <div className='flex justify-center'>
-        
           <div class='container '>
             <h2 class='text-center text-4xl lg:text-6xl  md:pt-12 pt-6'>
-            Product Offering
+              Product Offering
             </h2>
             <div className='object-cover p-4 md:p-12 flex  flex-col justify-center'>
+              <img
+                src={productEiv7}
+                alt=''
+                className='xl:px-72 lg:px-30 md:px-10'
+              />
 
-            <img src={productEiv7} alt="" className='xl:px-72 lg:px-30 md:px-10' />
-     
-            <div className='flex text-lg xl:px-72 md:px-10 lg:px-30 font-medium justify-between py-4 '>
-                  <h3>SWITCH IeV Series Product 1</h3>
-                  <a
-                    href='#product1'
-                    className='cursor-pointer text-blue-700 underline'
-                  >
-                    Learn More
-                  </a>
-             
+              <div className='flex text-lg xl:px-72 md:px-10 lg:px-30 font-medium justify-between py-4 '>
+                <h3>SWITCH IeV Series Product 1</h3>
+                <a
+                  href='#product1'
+                  className='cursor-pointer text-blue-700 underline'
+                >
+                  Learn More
+                </a>
+              </div>
             </div>
-            
-          </div>
           </div>
         </div>
+      </section>
+
+      <section>
+        <div className='w-[500px] h-[280px] mx-auto bg-slate-200'></div>
+        <ImageSlider slides={slides} />
       </section>
     </div>
   );
